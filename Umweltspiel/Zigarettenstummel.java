f6c76bfbefbd7137f0a6d629b292d88d7eb18651
Hanna <hanna.sommer.hs@gmail.com>
2021-03-09 11:05:26 +0100

    Greenfoot aktualisiert

---
+++ Umweltspiel/Zigarettenstummel.java
@@ -0,0 +1,25 @@
+/**
+ * Write a description of class Zigarettenstummel here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Zigarettenstummel  
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Zigarettenstummel
+     */
+    public Zigarettenstummel()
+    {
+      if(Greenfoot.mouseClicked(this))
+         showText ("Tipp");
+
+      return true;
+      
+      
+    }
+   
+}
