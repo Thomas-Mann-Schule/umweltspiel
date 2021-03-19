b2965da43c175bdd4c1ea4911a59743edd3a00df
Marie <schultz_marie@gmx.de>
2021-03-19 08:59:09 +0100

    Aktualisiert

---
+++ Umweltspiel/Umweltspiel Greenfoot/Umweltspiel-copy/Objekt3.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Objekt3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Objekt3 extends Müllobjekte
+{
+    /**
+     * Act - do whatever the Objekt3 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
